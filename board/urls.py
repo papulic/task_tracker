cf8e197ffff2abe6a46f1aadf51acb9b900339be
papulic <papulic@gmail.com>
2017-03-29 14:11:52 +0200

    Update urls.py

--- board/urls.py
+++ board/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     url(r'^(?P<project_id>[0-9]+)/create_note/$', views.create_note, name='note-add'),
     url(r'^(?P<project_id>[0-9]+)/update/(?P<note_id>[0-9]+)/$', views.note_update, name='note-update'),
     url(r'^(?P<project_id>[0-9]+)/delete/$', views.delete_project, name='project-delete'),
+    url(r'^(?P<project_id>[0-9]+)/delete_all/$', views.delete_all, name='note-delete-all'),
     url(r'^(?P<project_id>[0-9]+)/delete/(?P<note_id>[0-9]+)/$', views.note_delete, name='note-delete'),
     url(r'^login_user/$', views.login_user, name='login_user'),
     url(r'^logout_user/$', views.logout_user, name='logout_user'),
